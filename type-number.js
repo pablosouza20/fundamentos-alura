7df6dd84bf8ea16b5ee3f93f8f2e84a4de914453
pablosouza20 <Pablosouza09@outlook.com.br>
2023-02-24 17:26:22 -0300

    exemplos da aula 01

---
+++ type-number.js
@@ -0,0 +1,17 @@
+// tipo Number
+
+const num = 7;
+const numerico = 41;
+const op = num + numerico;
+console.log(op)
+
+// ponto flutuante
+const numeroPontoFlutuante = 3.3;
+const ponto = 4.4;
+const pontoFlutuante = numeroPontoFlutuante * ponto;
+console.log(pontoFlutuante)
+
+
+// NaN -> Not A Number (não é um número)
+const exemplo = "Alura";
+console.log(exemplo * ponto)
---
+++ type-string.js
@@ -0,0 +1,25 @@
+// type Strings
+
+const texto1 = "Olá mundo";
+const texto2 = "JavaScript";
+const stringDeNumeros = "32541";
+console.log(stringDeNumeros);
+
+// concatenação (+)
+console.log(texto1 + " do " + texto2);
+
+// citação
+const citacao = "'O Javascript é utilizado na Web'";
+console.log(citacao)
+
+
+// Forma de validar 2 strings
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+const inputMinusculo = cidade.toLowerCase();
+console.log(cidade === inputMinusculo);
+
+
+// Quantidade de strings
+const Senha = "minhaSenha123";
+console.log(Senha.length);
